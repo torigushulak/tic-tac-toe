a450508c40f7c11f3e1fd810da2503606aff53a3
Tori Gushulak <torigushulak@Toris-MacBook-Air.local>
2022-11-05 22:30:21 -0600

    game logic works

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 
-// Worth keeping?
-const displayController = (() => {
-    console.log("Display controller");
-})();
-
-
 const playerFactory = (symbol) => {
     const getSymbol = () => { return symbol }
     return { getSymbol };
@@ -19,34 +13,97 @@ const game = (() => {
     ]
 
     let turn = 0;
-
-
+    let scoreX = 0;
+    let scoreO = 0;
     let playerX = playerFactory('X');
     let playerO = playerFactory('O');
 
-    // Module to render the gameboard on init
+
+    // Closure to render the gameboard on init 
     let render = () => {
         let gameboardElement = document.getElementById("gameboardElement");
         gameboardElement.innerHTML = '';
+        document.getElementById('xScore').textContent = scoreX;
+        document.getElementById('oScore').textContent = scoreO;
         gameboard.forEach((cell, index) => {
             const cellElement = document.createElement('div');
             cellElement.classList.add("cellStyle")
             cellElement.textContent = gameboard[index];
             cellElement.addEventListener('click', () => {
+
+
                 // determine active player
                 // statement ? if true do this : else do this
                 const activePlayer = (turn % 2 == 0) ? playerX : playerO;
 
-                // assign & render active player symbol
-                gameboard[index] = activePlayer.getSymbol();
-                render();
+                // check if a symbol is assigned already 
+                if (gameboard[index] == null) {
+                    // assign & render active player symbol
+                    gameboard[index] = activePlayer.getSymbol();
+                    render();
+                    turn++;
+                    // increment turn
+                }
 
-                // increment turn
-                turn++;
+                // check if someone has won
+                if (checkWin(gameboard)) {
+
+                    if (activePlayer.getSymbol() == "X") {
+                        scoreX++;
+                    } else {
+                        scoreO++;
+                    }
+
+                    console.log("End game");
+                    // reset turns
+                    turn = 0;
+                    // reset gameboard
+                    gameboard = [
+                        null, null, null,
+                        null, null, null,
+                        null, null, null,
+                    ]
+
+                }
+                // check if turns ran out
+                else if (turn == 8) {
+                    console.log("End game. Tie!");
+                    // reset turns
+                    turn = 0;
+                    // reset gameboard
+                    gameboard = [
+                        null, null, null,
+                        null, null, null,
+                        null, null, null,
+                    ]
+
+                }
+
             })
             gameboardElement.appendChild(cellElement);
         })
     };
+
+    /*
+    0 1 2
+    3 4 5
+    6 7 8
+    */
+
+    const checkWin = (gb) => {
+        // cols
+        if (gb[0] != null && gb[0] == gb[3] && gb[3] == gb[6]) return true;
+        else if (gb[1] != null && gb[1] == gb[4] && gb[4] == gb[7]) return true;
+        else if (gb[2] != null && gb[2] == gb[5] && gb[5] == gb[8]) return true;
+        else if (gb[0] != null && gb[0] == gb[1] && gb[1] == gb[2]) return true;
+        else if (gb[3] != null && gb[3] == gb[4] && gb[4] == gb[5]) return true;
+        else if (gb[6] != null && gb[6] == gb[7] && gb[7] == gb[8]) return true;
+        else if (gb[0] != null && gb[0] == gb[4] && gb[4] == gb[8]) return true;
+        else if (gb[2] != null && gb[2] == gb[4] && gb[4] == gb[6]) return true;
+
+        else return false;
+    };
+
     render();
 })();
 
